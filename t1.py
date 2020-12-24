01db39ea537ca06693a2078ac4a69ca12e7f48d1
Alejandro García Surió <a.garcia.surio@icloud.com>
2020-12-24 12:46:25 +0100

    Añadido las funciones de Tiempo

--- t1.py
+++ t1.py
@@ -2,6 +2,8 @@ import Tiempo as tp
 
 nuevo = tp.Tiempo()
 nuevo.imprimirFecha()
+nuevo.imprimirHora()
 nuevo.imprimirTodo()
+nuevo.abrirArchivo()
 
 #print("Hoy es %i del %i del %i (%i/%i/%i) y son las %i:%i:%i. Es el %i dia de la semana y el %i dia del año." % (local[2], local[1], local[0], local[2], local[1], local[0], local[3], local[4], local[5], local[6] + 1, local[7]))
