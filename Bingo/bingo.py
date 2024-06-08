dd596c7d8d0ced3b393f423adbd271bd826c46e6
Alejandro <alejandro.g.surio@outlook.es>
2024-06-08 20:11:05 +0200

    Main loop control added

--- Bingo/bingo.py
+++ Bingo/bingo.py
@@ -2,11 +2,25 @@ import random
 import Card_board as cb
 import Menu as menu
 
+main_loop_flag = True
 
-menu.main_menu()
+while main_loop_flag:
+    menu.main_menu()
+    user_main_input = input("Cual es tu opcion: ")
+
+    if user_main_input == "1":
+        user_card = cb.Card_board()
+        user_card.printCard()
+    elif user_main_input == "2":
+        print("Has ganado la ostia de partidas.")
+    elif user_main_input == "3":
+        user_name = input("Cual es tu nombre de usuario: ")
+    elif user_main_input == "4":
+        print("Adios y que te peten.")
+        main_loop_flag = False
+    else:
+        print("Ni idea de lo que has puesto.")
 
 numbers_stock = list(range(0,51))
 random.shuffle(numbers_stock)
 
-user_card = cb.Card_board()
-user_card.printCard()
