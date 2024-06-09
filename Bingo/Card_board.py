1e49aeb3476546e1842df32ac132908dc357235f
Alejandro Surio <alejandro.g.surio@outlook.es>
2024-06-10 00:01:04 +0200

    Añadido el juego principal, algunos menus y funciones en cardboard

--- Bingo/Card_board.py
+++ Bingo/Card_board.py
@@ -2,6 +2,9 @@ import random
 
 class Card_board:
     def __init__(self) -> None:
+        pass
+
+    def create_card_board(self):
         self.numbers = list(range(0, 51))
         random.shuffle(self.numbers)
         self.numbers = self.numbers[0:20]
@@ -12,11 +15,21 @@ class Card_board:
         print("\t----------------------------")
 
         for index, num in enumerate(self.numbers):
-
-            if num < 10:
+            if num == "X":
+                print(f"\t  {num}", end=" ")
+            elif num < 10:
                 print(f"\t  {num}", end=" ")
             else:
                 print(f"\t {num}", end=" ")
 
             if((index + 1) % 4 == 0):
                 print()
+
+    def mark_number(self, number) -> bool:
+        try:
+            position = self.numbers.index(int(number))
+            self.numbers[position] = "X"
+            return True
+        except ValueError:
+            return False
+        
--- Bingo/bingo.py
+++ Bingo/bingo.py
@@ -1,11 +1,64 @@
 import random
+import os
 import Card_board as cb
 import Menu as menu
 import User as user
 
+def startGame():
+    # Generate the random stock number
+    numbers_stock = list(range(0,51))
+    random.shuffle(numbers_stock)
+
+    # Generate user card
+    player_card.create_card_board()
+
+    while len(numbers_stock) > 0:
+        print()
+        print("¡Partida en curso!")
+        print("------------------")
+        ball = numbers_stock.pop()
+        print(f"Bola número {ball}")
+
+        if len(numbers_stock) == 0:
+            print(f"¡Se acabaron los números, fin de la partida!")
+        else:
+            print(f"Quedan {len(numbers_stock)} bolas.")
+
+        print()
+        player_card.printCard()
+        print()
+
+        print("Que quieres hacer?")
+        print("1. Marcar el número en el cartón.")
+        print("2. Cantar línea.")
+        print("3. Cantar bingo.")
+        print("4. Pasar.")
+        print("5. Abandonar la partida.")
+        selection = input("Tu selección: ")
+
+        if selection == "1":
+            if player_card.mark_number(ball):
+                print("¡Numero marcado!")
+            else:
+                print("Tu número no esta en tú cartón.")
+        elif selection == "2":
+            print("¡Línea!")
+        elif selection == "3":
+            print("¡Bingo, bingo!")
+        elif selection == "4":
+            print("No haces nada y esperas.")
+        elif selection == "5":
+            print("Saliendo de la sala.")
+            break
+        else:
+            print("No te entiendo, pasas turno.")
+
+
+
 main_loop_flag = True
 first_time_playing = True
 player = user.User()
+player_card = cb.Card_board()
 
 while main_loop_flag:
     
@@ -18,8 +71,7 @@ while main_loop_flag:
     user_main_input = input("Cual es tu opcion: ")
 
     if user_main_input == "1":
-        user_card = cb.Card_board()
-        user_card.printCard()
+        startGame()
     elif user_main_input == "2":
         player.statistics()
     elif user_main_input == "3":
@@ -29,7 +81,3 @@ while main_loop_flag:
         main_loop_flag = False
     else:
         print("Ni idea de lo que has puesto.")
-
-numbers_stock = list(range(0,51))
-random.shuffle(numbers_stock)
-
