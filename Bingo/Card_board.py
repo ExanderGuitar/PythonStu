739fcbc5328a173e9bc96086a9d8614d55d9d207
Alejandro Surio <alejandro.g.surio@outlook.es>
2024-06-16 15:07:52 +0200

    Changes in Card_board

--- Bingo/Card_board.py
+++ Bingo/Card_board.py
@@ -7,23 +7,27 @@ class Card_board:
     def create_card_board(self):
         self.numbers = list(range(0, 51))
         random.shuffle(self.numbers)
-        self.numbers = self.numbers[0:20]
+        self.numbers = self.numbers[0:25]
+        self.lines = [self.numbers[0:5], self.numbers[5:10], 
+                      self.numbers[10:15], self.numbers[15:20],
+                      self.numbers[20:25]]
+        for n in range(5):
+            self.lines[n][random.randrange(5)] = "X"
 
     def printCard(self):
-        print("\t    Bingo Amigo Oro Feliz ")
-        print("\t       Carton usuario ")
-        print("\t----------------------------")
+        print("\t       Bingo Amigo Oro Feliz        ")
+        print("\t           Carton usuario           ")
+        print("\t------------------------------------")
 
-        for index, num in enumerate(self.numbers):
-            if num == "X":
-                print(f"\t  {num}", end=" ")
-            elif num < 10:
-                print(f"\t  {num}", end=" ")
-            else:
-                print(f"\t {num}", end=" ")
-
-            if((index + 1) % 4 == 0):
-                print()
+        for line in self.lines:
+            for index, num in enumerate(line):
+                if num == "X":
+                    print(f"\t  {num}", end=" ")
+                elif num < 10:
+                    print(f"\t  {num}", end=" ")
+                else:
+                    print(f"\t {num}", end=" ")
+            print()
 
     def mark_number(self, number) -> bool:
         try:
